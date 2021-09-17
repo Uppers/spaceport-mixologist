05998ef82df25161a140ad08e8454374aeb1e8fe
Uppers <merchantfx@hotmail.com>
2021-09-17 19:41:40 +0800

    final commit before merging branch with main

--- state.py
+++ state.py
@@ -4,7 +4,6 @@ class State():
     def __init__(self):
         self.running = True # the program is running
         self.score = 0
-        self.exploded = False 
         self.menu_state = MenuPage(self)
         self.game_state = GamePage(self)
         self.game_over_state = GameOverPage(self)
